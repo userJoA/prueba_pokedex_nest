986392e39c2de762f139e289148d2f6b3dec9cd4
Gallardo Alejandro Joaquin <97065359+userJoA@users.noreply.github.com>
2026-01-27 06:25:05 -0300

    Antes de insertar por lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 
 
@@ -15,6 +16,7 @@ import { CommonModule } from './common/common.module';
     PokemonModule,
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokedex'),
     CommonModule,
+    SeedModule,
   ],
 })
 export class AppModule {}
---
+++ src/seed/seed.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
+  async executeSeed() {
+    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=15');
+    data.results.forEach( ({name, url}) => {
+        const segments = url.split('/');
+        console.log(segments);
+        const no = +segments[segments.length -2];
+        console.log({name, no});
+    })
+    return data.results;
+  }
+}
